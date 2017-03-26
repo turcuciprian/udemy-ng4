0b681006502a499a853ec15ea659983bf29fe970
Ciprian Turcu <turcuciprian1@gmail.com>
2017-03-26 23:07:53 +0300

    Added page1 and page2 components, added routing, routed each component accourdingly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { Page1Component } from './page1.component';
+import { Page2Component } from './page2.component';
 
+const appRoutes: Routes = [
+  {
+    path:'', component: Page1Component
+  },
+  {
+    path:'page1', component: Page1Component
+  },
+  {
+    path:'page2', component: Page2Component
+  },  
+]; 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
